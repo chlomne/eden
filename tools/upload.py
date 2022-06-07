1638f1fd1ad240f5084d5cb4e18add69dcd16c44
Chloe Newmark <chloe.m.newmark@gmail.com>
2022-06-07 15:54:33 -0500

    feat: upload script works with macOS shortcuts

--- tools/upload.py
+++ tools/upload.py
@@ -3,8 +3,6 @@ import random
 import requests
 import mimetypes
 import string
-import sys
-import json
 import utils
 import os
 
@@ -13,7 +11,7 @@ utils.load_env()
 SUPABASE_TOKEN = os.getenv("SUPABASE_TOKEN")
 ROOT = "https://yhnhyvjuksphwndbgwtq.supabase.co/storage/v1/object/-/"
 
-file_path = sys.argv[1]
+file_path = input("")
 
 def generate_id(length: int):
   return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
@@ -29,4 +27,5 @@ with open(file_path, 'rb') as file:
   file_content = file.read()
   response = requests.post(f'{ROOT}/{generate_id(6)}{extension}', data=file_content, headers={'Content-Type': mimetype, 'Cache-Control': 'max-age=86400', 'Authorization': f'Bearer {SUPABASE_TOKEN}'})
   
-  print(json.dumps(response.json(), indent=4))
+  key = response.json()['Key']
+  print(f'https://cdn.chlomne.workers.dev/{key}')
